f9a2411b0350105e444106d8f9ff682c60f7d993
manish211108 <manishhexa2111@gmail.com>
2025-07-08 21:04:56 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script async custom-element="amp-auto-ads"
+          src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js">
+        </script>
         <meta name="google-adsense-account" content="ca-pub-3136730634801361" />
       </head>
       <body>{children}
+        <amp-auto-ads type="adsense"
+          data-ad-client="ca-pub-3136730634801361">
+        </amp-auto-ads>
         {/* Footer will be rendered in the main page, not here, to access setCurrentView */}
       </body>
     </html>
